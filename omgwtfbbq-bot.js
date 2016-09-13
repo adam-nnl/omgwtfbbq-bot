72eaa33c8b0591a92cf47a298ea05c9bd2843655
Adam Lara <adam.nnl@gmail.com>
2016-09-12 23:39:32 -0500

    Update omgwtfbbq-bot.js

--- omgwtfbbq-bot.js
+++ omgwtfbbq-bot.js
@@ -1,1 +1,22 @@
 //hai
+
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var Botkit = require('./lib/Botkit.js');
+var natural = require('natural');
+
+var controller = Botkit.slackbot({
+    debug: true
+});
+
+var bot = controller.spawn({
+    token: process.env.token
+}).startRTM();
+
+
+controller.hears('','ambient,direct_message,direct_mention,mention',function(bot,message) {  
+
+})
