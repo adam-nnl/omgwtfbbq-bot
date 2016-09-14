5cfbcff2b1f432451ba6d22d154d1158515bd46a
Adam Lara <adam.nnl@gmail.com>
2016-09-13 22:56:58 -0500

    Update omgwtfbbq-bot.js

--- omgwtfbbq-bot.js
+++ omgwtfbbq-bot.js
@@ -20,7 +20,7 @@ var bot = controller.spawn({
 //list for EVERYTHING, run message test through natural NLP to filter down to commands? or something? accept/deny/intent/request?
 controller.hears('','ambient,direct_message,direct_mention,mention',function(bot,message) {
     var natural = require('natural'),
-    this.classifier = new natural.BayesClassifier();
+    classifier = new natural.BayesClassifier();
     this.classifier.load('corpus.json', null, function(err, classifier) {
     console.log(classifier.classify('long SUNW'));
     console.log(classifier.classify('short SUNW'));
