7a7ac7365a852760e80d53212d22c33e1fa91360
Adam Lara <adam.nnl@gmail.com>
2016-09-13 22:32:41 -0500

    Update omgwtfbbq-bot.js

--- omgwtfbbq-bot.js
+++ omgwtfbbq-bot.js
@@ -19,7 +19,7 @@ var bot = controller.spawn({
 
 //list for EVERYTHING, run message test through natural NLP to filter down to commands? or something? accept/deny/intent/request?
 controller.hears('','ambient,direct_message,direct_mention,mention',function(bot,message) {
-    this.classifier = new NLP.LogisticRegressionClassifier();
+    this.classifier = new natural.LogisticRegressionClassifier();
     var msg = message.text;
     classifier.load('corpus.json', null, function(err, classifier) {
         bot.reply(message, classifier.classify(msg));
