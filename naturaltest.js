6858626ed8bad785f083676d6d6a56758e13ebb8
Adam Lara <adam.nnl@gmail.com>
2016-09-13 23:18:02 -0500

    Update naturaltest.js

--- naturaltest.js
+++ naturaltest.js
@@ -1,5 +1,6 @@
 var natural = require('natural'),
-  classifier = new natural.BayesClassifier();
+    classifier = new natural.BayesClassifier();
+
 natural.BayesClassifier.load('corpus.json', null, function(err, classifier) {
-    console.log(classifier.classify('testing'));
-});
+	console.log(classifier.classify('did the tests pass?'));
+    });
